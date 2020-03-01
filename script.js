8b4521666c1534c99490164ccf7d7b6387c24476
NateeWhitee <59496066+NateeWhitee@users.noreply.github.com>
2020-03-01 17:34:38 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -9,3 +9,26 @@ function showMenu() {
         menuToggle += 1;
     }
 }
+
+function changeToVid(projectNumber) {
+    document.getElementById(`project_${projectNumber}`).innerHTML = `<video src="project_${projectNumber}.mp4" type="video/mp4" controls>Your browser does not support videos.</video>`;
+    console.log('Complete');
+}
+
+function closeFooter() {
+    document.getElementById('socialMedia').style.bottom = '-100%';
+}
+
+function goToSocialPage(socialPage) {
+    switch (socialPage) {
+        case 'instagram':
+            window.open('https://instagram.com/offical.mast.design?igshid=150os8b9zuge0', 'taget=_blank');
+            break;
+        case 'twitter':
+            break;
+        case 'facebook':
+            break;
+        case 'youtube':
+            break;
+    }
+}
